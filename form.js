3b10ddef4136f3e8e4188dba304846c7081c98f1
waire <waire.tega@gmail.com>
2021-06-14 16:56:25 +0100

    correct

--- form.js
+++ form.js
@@ -2,25 +2,47 @@ const form = document.forms[0];
 form.addEventListener("submit", submitFunc);
 function submitFunc(e) {
     e.preventDefault();
-    const url = form.action;
+    // const url = form.action;
+    const base_url = "http://localhost:10000/login"
     const loginDetails = {};
     loginDetails.email = form.useremail.value;
     loginDetails.password = form.userpassword.value;
     console.log(loginDetails);
-    fetch(url, {
+    fetch(base_url, {
         body: JSON.stringify(loginDetails),
         method: "POST",
+        //credentials: 'same-origin',
         mode: "no-cors",
         headers: {
+            // Accept: 'application/json',
             "Content-Type": "application/json",
-        }
+        },
         })
-        .then(res => res.json())
+        .then(res => {if (!res.ok) {
+            throw `Server error: [${res.status}] [${res.statusText}] [${res.url}]`;
+        }
+        return res.json()})
         .then(data => {
             console.log(data);
             // alert("success")
-        }).catch(error => {
-            console.log(error);
-            alert("error")
         })
+        .catch(error => {
+            console.log(error);
+            alert(error)
+        })
+        // const getForm = async() => {
+        //     const response = await fetch("http://127.0.0.1:8080/login", {
+        //         method: 'POST',
+        //         body: JSON.stringify(loginDetails),
+        //         mode:"no-cors",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         }
+        //       });
+        //       const string = await response.text();
+        //       const json = string === "" ? {} : JSON.parse(string);
+        //     //   return json;
+        //       console.log(json);
+        // }
+        // getForm();
     }
--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"os"
 )
 
 type Details struct {
@@ -16,27 +15,25 @@ type Details struct {
 
 var allData []Details
 
-// json, _ := ioutil.ReadAll(r.Body)
-// fmt.Println("Got params: ", string(json))
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	formData := &Details{}
-	defer r.Body.Close()
-
-	if err := json.NewDecoder(r.Body).Decode(formData); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	fmt.Println(*formData)
-	allData = append(allData, *formData)
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(formData); err != nil {
-		log.Printf("Can't encode %v - %s", w, err)
-	}
-	fmt.Println(allData)
+
+	var formData Details
+	_ = json.NewDecoder(r.Body).Decode(&formData)
+	allData = append(allData, formData)
+	json.NewEncoder(w).Encode(&formData)
+	fmt.Println("Got THE form: ", formData)
+	fmt.Println("Got ALL forms: ", allData)
+	fmt.Fprintf(os.Stdout, "Employee is %#v\n", formData)
+
 }
 func main() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/login", loginHandler).Methods("POST")
+	// myRouter := mux.NewRouter()
+	// myRouter.HandleFunc("/login", loginHandler).Methods("POST")
+	http.HandleFunc("/login", loginHandler)
+
 	fmt.Println("server is running")
-	log.Fatal(http.ListenAndServe(":8080", myRouter))
+	// log.Fatal(http.ListenAndServe(":10000", myRouter))
+	log.Fatal(http.ListenAndServe(":10000", nil))
+
 }
